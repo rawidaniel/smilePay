25011adf555e7681b89e7d25df0f00c58a0476aa
EbenGitHub <abenezergoo@gmail.com>
2023-09-30 18:25:34 +0300

    fix: added the rest of services

---
+++ locust/locustfile.py
@@ -0,0 +1,11 @@
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    # def on_start(self):
+    #     self.client.post("/login", json={"username":"foo", "password":"bar"})
+
+    @task
+    def status_check(self):
+        self.client.get("/status")
